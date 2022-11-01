516883b8cac37fdbbbc4a3a0d2c0505ff941ce3d
404 <fabrucemaminiaina@gmail.com>
2022-11-01 18:42:08 +0300

    mise en place page login

---
+++ src/Login.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Grid from '@mui/material/Grid'
+import './assets/css/Login.css'
+import pic from './assets/images/pp.png'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+
+
+export default function Login() {
+  return (
+    <div>
+        <Grid container spacing={0}>
+            <Grid item sx={12} md={6}>
+                <div className='leftSide'>
+                </div>
+            </Grid>
+            <Grid item sx={12} md={6}>
+                <div className='rightSide'>
+                    <h1 className='header'>Gestion de stock</h1>
+             
+                    <div className='form'>   <TextField id="standard-basic" label="Email" variant="standard" type="email" className='textField' />  </div>
+                    <div className='form'>   <TextField id="standard-basic" label="Mot de passe" variant="standard" type="password" className='textField' /> </div>
+                    <div className='form'>  <Button variant="contained" style={{backgroundColor: '#595FF1'}} className='btn'>    Se connecter
+                    </Button> </div>
+                  
+               
+                    
+                </div>
+            </Grid>
+          
+        </Grid>
+    </div>
+  )
+}
