9575099fad228d36734f602ba96f8961383ff114
404 <fabrucemaminiaina@gmail.com>
2022-11-12 14:13:10 +0300

    Update Dashboard.jsx

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -18,9 +18,11 @@ export default function Dashboard() {
             <div id="main-menu" class="main-menu collapse navbar-collapse">
                 <ul class="nav navbar-nav">
                     <li class="active">
-                        <a href="index.html"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
+                        <a href="index.html"><i class="menu-icon fa fa-laptop"></i>General </a>
                     </li>
-                    <li class="menu-title">UI elements</li>{/* <!-- /.menu-title --> */}
+                    <li class="active">
+                        <a href="#" onClick={ navigate('/ajoutArticle')}><i class="menu-icon fa fa-laptop"></i>Ajout Article </a>
+                    </li>
                     <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-cogs"></i>Components</a>
                         <ul class="sub-menu children dropdown-menu">                            <li><i class="fa fa-puzzle-piece"></i><a href="ui-buttons.html">Buttons</a></li>
@@ -92,6 +94,8 @@ export default function Dashboard() {
             </div>{/* <!-- /.navbar-collapse --> */}
         </nav>
     </aside>
+
+
     <div id="right-panel" className="right-panel">
     <header id="header" class="header">
             <div class="top-left">
@@ -123,6 +127,7 @@ export default function Dashboard() {
                 </div>
             </div>
         </header>
+        
       <div className="content">
         <div className="row">
         <div class="col-lg-3 col-md-6">
